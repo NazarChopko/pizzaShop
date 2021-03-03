538d6d989ed150e68af7329e3154fefadaa3dcff
Nazar <67974209+NazarChopko@users.noreply.github.com>
2021-03-03 20:02:33 +0200

    commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,73 @@
+import React,{useCallback,useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {Categories,SortPopup,PizzaBlock,PizzaLoadingBlock} from '../components'
+
+import {setCategory,setSortBy} from '../redux/actions/filters'
+import {fetchPizzas } from '../redux/actions/pizzas'
+import {addPizzaToCart } from '../redux/actions/cart'
+
+const categoryNames = ['Мясные','Вегетарианская','Гриль','Острые','Закрытые',]
+const sortItems =[
+  {name:'популярности',type:'rating',order:'desc'},
+  {name:'цена', type:'price',order:'desc'},
+  {name:'алфавит',type:'name',order:'asc'},]
+
+
+function Home() {
+
+  const dispatch = useDispatch()
+  const items = useSelector(({pizzas}) => pizzas.items);
+  const cartItems = useSelector(({cart}) => cart.items);
+  const isLoaded = useSelector(({pizzas}) => pizzas.isLoaded);
+  const {category,sortBy} = useSelector(({filters}) => filters);
+
+  const onSelectCategory = useCallback((index)=>{
+    dispatch(setCategory(index));
+  },[]);
+
+  const onSelectSortType = useCallback((type)=>{
+    dispatch(setSortBy(type));
+  },[]);
+
+  useEffect(()=>{
+    
+      dispatch(fetchPizzas(sortBy,category))
+    
+  }
+  , [category,sortBy]);
+
+  const handleAddPizzaToCart = (obj)=>{
+      dispatch(addPizzaToCart(obj))
+  }
+  
+  console.log(items)
+
+    return (
+        <div className="container">
+        <div className="content__top">
+         <Categories 
+         activeCategory={category}
+         onClickCategory={onSelectCategory} 
+         items={categoryNames}/>
+          <SortPopup  activeSortType={sortBy.type} items={sortItems} onClickSortType={onSelectSortType}/>
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {isLoaded 
+          ? items.map((obj)=>(<PizzaBlock 
+            onClickAddPizza={handleAddPizzaToCart} 
+            key={obj.id} 
+            addedCount={cartItems[obj.id] && cartItems[obj.id].items.length} {...obj}/>)) 
+          : Array(10).fill(0).map((_,index)=><PizzaLoadingBlock key={index}/>)}
+         
+       </div>
+  </div>
+
+
+        
+      
+    
+    )
+}
+
+export default Home
---
+++ src/redux/actions/pizzas.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+
+export const setLoaded = payload => {
+    return {
+        type:'SET_LOADED',
+        payload,
+    }
+}
+
+export const fetchPizzas = (sortBy,category) => (dispatch) => {
+    dispatch(setLoaded(false))
+    axios.get(`/pizzas?${category !== null ? `category=${category}` :''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({data}) => {
+     dispatch(setPizzas(data))
+});
+};
+
+
+export const setPizzas = (items) =>{
+    return {
+        type:'SET_PIZZAS',
+        payload:items,
+    }
+}
+
